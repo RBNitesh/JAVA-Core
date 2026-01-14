5e81885d3899d2acc4f39d00f6b846be98a5b3c6
Nitesh Kumar <nk41521391@gmail.com>
2026-01-14 15:04:08 +0530

    new file

--- JAVA8_features/WildCards/Main.java
+++ JAVA8_features/WildCards/Main.java
@@ -14,6 +14,11 @@ class EvenNumber extends Number {
     }
 }
 
+protected class Box {
+     int value; 
+}
+
+
 public class Main{
     public static void main(String[] args) {
         List<Integer> li = Arrays.asList(1, 2, 3, 4);
