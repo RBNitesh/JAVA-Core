25476cb37b49a379c9dbdd57ca6a09b714e786e2
Nitesh Kumar <nk41521391@gmail.com>
2026-01-19 23:40:50 +0530

    file has been modified

--- ReflectionAPI/Main.java
+++ ReflectionAPI/Main.java
@@ -1,5 +1,10 @@
 public class Main{
-    public static void main(String strArr[]){
+    public static void main(String abc[]){
         System.out.println("Program is running...");
+        
+        for (String string : abc) {
+            System.out.print(string + " ");
+        }
+        System.out.println();
     }
 }
