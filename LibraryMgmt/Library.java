e7855ee24f4c8cd29337c41212b10c79c2a8fc9f
Nitesh Kumar <nk41521391@gmail.com>
2026-02-17 23:34:20 +0530

    intial change

--- LibraryMgmt/Library.java
+++ LibraryMgmt/Library.java
@@ -11,11 +11,62 @@ public class Library {
         System.out.println(
             "                  Select From The Following Options:               ");
         System.out.println(
-           "**********************************************************************");
+                "**********************************************************************");
              
-        Books books = new Books();
-        Students studentsObj = new Students();
+        // creating obj of books class
+        Books ob = new Books();
+
+        // creating obj of students class
+        Students obStudents = new Students();
 
         int choice, searchChoice;
+
+        do{
+            ob.dispMenu();
+            choice = in.nextInt();
+
+            switch (choice) {
+                case 1:
+                    Book b = new Book();
+                    ob.addBook(b);
+                    break;
+                case 2:
+                    ob.upgradeBookQty();
+                    break;
+                case 3:
+                    System.out.println(
+                        " press 1 to Search with Book Serial No.");
+                    System.out.println(
+                            " Press 2 to Search with Book's Author Name.");
+                    searchChoice = in.nextInt();
+
+                    switch (searchChoice) {
+                        case 1:
+                            ob.searchBySNo();
+                            break;
+                        case 2:
+                            ob.searchByAuthorName();
+                    }
+                    break;
+                case 4:
+                    ob.showAllBooks();
+                    break;
+                case 5:
+                    Student s = new Student();
+                    obStudents.addStudent(s);
+                    break;
+                case 6:
+                    obStudents.showAllStudents();
+                    break;
+                case 7:
+                    obStudents.checkOutBook(ob);
+                    break;
+                case 8:
+                    obStudents.checkInBook(ob);
+                default:
+                    System.out.println("Enter 0 to 8");
+            }
+        }
+        while (choice != 0);
     }
 }
--- LibraryMgmt/Students.java
+++ LibraryMgmt/Students.java
@@ -9,7 +9,7 @@ public class Students{
     public void addStudent(Student student) {
         for (int i = 0; i < count; i++) {
             if (thStudents[i].regNo == student.regNo) {
-                System.out.println("Student with registration number " + regNo + " already exist in record.");
+                System.out.println("Student with registration number " + student.regNo + " already exist in record.");
                 return;
             }
         }
@@ -42,6 +42,58 @@ public class Students{
     }
 
     public void checkOutBook(Books book) {
-        
+        int studentIndex = this.isStudent();
+
+        if (studentIndex != -1) {
+            System.out.println("Checking out");
+
+            book.showAllBooks();
+            Book b = book.checkOut(); // checked out book
+
+            System.out.println("Checking out");
+            if (b != null) {
+                if (thStudents[studentIndex].booksCount < 3) {
+                    System.out.println("adding book");
+                    // add the checked out book in student account
+                    thStudents[studentIndex].borrowedBooks[thStudents[studentIndex].booksCount] = b;
+                    thStudents[studentIndex].booksCount++;
+                    return;
+                }
+                System.out.println("Student can't borrow more than three books.");
+            }
+
+            // not available
+            System.out.println("Book is not available.");
+        }
+    }
+    
+    public void checkInBook(Books book) {
+        int studentIndex = this.isStudent();
+
+        if (studentIndex != -1) {
+            // printing credentials corresponding to student
+            System.out.println("SNo\t\tBook Name\t\tAuthor Name");
+            Student student = thStudents[studentIndex];
+
+            for (int i = 0; i < student.booksCount; i++) {
+                System.out.println(
+                        student.borrowedBooks[i].sNo + "\t\t"
+                                + student.borrowedBooks[i].bookName + "\t\t"
+                                + student.borrowedBooks[i].authorName);
+            }
+
+            System.out.print("Enter Serial number of book to be checked in: ");
+            int sNo = in.nextInt();
+
+            for (int i = 0; i < student.booksCount; i++) {
+                if (sNo == student.borrowedBooks[i].sNo) {
+                    book.checkIn(student.borrowedBooks[i]);
+                    student.borrowedBooks[i] = null;
+                    return;
+                }
+            }
+
+            System.out.println("Book of Serial Number " + sNo + " not Found");
+        }
     }
 }
